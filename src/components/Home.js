5fcf3344540b7958fb288fc8d9898852b3e05ec6
Aarti <aartikadam639@gmail.com>
2022-06-21 16:42:11 +0530

    commit1

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div class="card bg-dark text-white border-0">
+        <img src="/Image/pan.jpeg" alt="loading" />
+        <div class="card-img-overlay d-flex flex-column justify-content-center ">
+          <div className="container">
+            <h5 class="card-title display-3 fw-bolder mb-0">New Season Arrivals</h5>
+            <p class="card-text">
+             CHECK OUT ALL THE NEW TRENDS
+            </p>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
